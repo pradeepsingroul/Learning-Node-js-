57b90b10183bcbd6164a46580557cb61cced791b
predeep kumar singroul <prasingroul4899@gmail.com>
2023-04-24 19:18:36 +0530

    node modules package

--- Day_2/Index.js
+++ Day_2/Index.js
@@ -4,14 +4,14 @@
 // -- global - we dont need to import it
 // -- non global --  we need to import it
 
-/*const fileSystem = require('fs');
+const fileSystem = require('fs');
 const fs = require('fs').writeFileSync;
-fileSystem.writeFileSync("introFile","my name is pradeep i am learnign node js andi am making txt file using node js")
-fs("introFile1","my name is pradeep i am learnign node js andi am making txt file using node js")*/
+fileSystem.writeFileSync("introFile.txt","my name is pradeep i am learnign node js andi am making txt file using node js")
+fs("introFile1.txt","my name is pradeep i am learnign node js andi am making txt file using node js")
 
 
 const colors = require('colors')
-console.log("installed colors package".bgBlue)
+console.log("installed colors package".red)
 
 
 
