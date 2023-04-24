568a07768a4f5e052855add6ddc86b3ac7e84dba
predeep kumar singroul <prasingroul4899@gmail.com>
2023-04-24 21:29:42 +0530

    data.js

---
+++ Day_3_make_a_simple_API/data.js
@@ -0,0 +1,18 @@
+const data = [
+        {
+        name:"Anhoti singroul",
+        email:'Anhoti@gmail.com',
+        city: "satna mp"
+    },
+    {
+        name:"Somvati singroul",
+        email:'Somvati@gmail.com',
+        city: "satna mp"
+    },
+    {
+        name:"pradeep singroul",
+        email:'prasingroul4899@gmail.com',
+        city: "satna mp"
+    }
+]
+module.exports  = data;
--- Day_3_make_a_simple_API/index.js
+++ Day_3_make_a_simple_API/index.js
@@ -1,16 +1,18 @@
 const http = require('http')
 const colors = require('colors')
+const data = require('./data')
 
-let object = {
-    name:"pradeep singroul",
-    email:'prasingroul4899@gmail.com',
-    city: "satna mp"
-}
-
+// let object = {
+//     name:"pradeep singroul",
+//     email:'prasingroul4899@gmail.com',
+//     city: "satna mp"
+// }
+let HTTPCODE = 200;
 http.createServer((req,resp)=>{
-    resp.writeHead(200,{'Content-Type':'application\json'});
-    resp.write(JSON.stringify(object))
+    resp.writeHead(HTTPCODE,{'Content-Type':'application\json'});
+    resp.write(JSON.stringify(data))
     resp.end();
 }).listen(4500)
+//localhost:4500 api
 
 console.log("pradeep singroul".red)
